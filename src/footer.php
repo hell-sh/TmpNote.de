0931ef187194af80ac6ea38776a33b9dc920b3a7
timmyRS <github@timmyrs.de>
2018-08-29 11:27:02 +0000

    Update copyright notice

--- src/footer.php
+++ src/footer.php
@@ -3,7 +3,7 @@
 			<button class="uk-modal-close-default" type="button" uk-close></button>
 			<h2 class="uk-modal-title">Open Source</h2>
 			<ul>
-				<li><a href="https://github.com/hellshltd/TmpNote.de" target="_blank">Source code</a></li>
+				<li><a href="https://github.com/hell-sh/TmpNote.de" target="_blank">Source code</a></li>
 				<li>Editor: <a href="https://ace.c9.io/" target="_blank">Ace</a></li>
 				<li>Crypto library: <a href="https://code.google.com/archive/p/crypto-js" target="_blank">crypto-js</a></li>
 			</ul>
